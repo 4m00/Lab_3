2324afcb29855d7cf901ce0d92ac19ef80a10a79
User <User@DESKTOP-RKJJ4L5>
2022-12-04 08:39:01 +0300

    Добавьте файлы проекта.

---
+++ Lab3/Lab3.py
@@ -0,0 +1,9 @@
+print("print IP:")
+import re
+pattern = re.compile(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+def isValid(IP):
+    if re.match(pattern, IP):
+        print("Correct IP")
+    else:
+        print("Incorrect IP")
+isValid(input())
